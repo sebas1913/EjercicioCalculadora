ff697d2050cb23bde8067c2dbf98fc112d6ae4cf
Juan Diego <juanzr1015@gmail.com>
2024-05-09 10:57:02 -0500

    Funcion Multiplicar

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function calculadora(num1, num2, op){
+    switch(op){
+        case '+':
+            return sumar(num1, num2);
+        case '-':
+            return restar(num1, num2);
+        case '*':
+            return multiplicar(num1, num2);
+        case '/':
+            return dividir(num1, num2)
+        default:
+            return 'Opción inválida'
+    }
+}
+
+function sumar(num1,num2){
+    return num1 + num2
+}
+
+function multiplicar(num1,num2){
+    return num1 * num2;
+}
