6740ce1cf44d2180f461bace31db427e26b972cc
sebas1913 <sebasosornicastrillon@gmail.com>
2024-05-09 11:32:17 -0500

    Corrijo rama Isa

---
+++ calculadora.js
@@ -0,0 +1,36 @@
+function calculadora(num1, num2, op){
+    switch(op){
+        case '+':
+            return sumar(num1, num2);
+        case '-':
+            return restar(num1, num2);
+        case '*':
+            return multiplicar(num1, num2);
+        case '/':
+            return dividir(num1, num2)
+        default:
+            return 'Opción inválida'
+    }
+}
+
+function sumar(num1,num2){
+    return num1 + num2
+}
+
+function restar(num1,num2){
+    return num1-num2
+}
+
+function division(num1,num2){
+    if (num2 !== 0){
+        return num1 / num2;
+    }
+    else{
+        return "No se puede dividir entre cero";
+    }
+}
+
+function multiplicar(num1,num2){
+    return num1 * num2;
+}
+
