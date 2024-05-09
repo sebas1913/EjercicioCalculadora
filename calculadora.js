90a9938296c25d2faa040f906de656aaf0d783f5
sebas1913 <sebasosornicastrillon@gmail.com>
2024-05-09 10:35:11 -0500

    Se agrega el código de la función

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+function calculadora(num1, num2, op){
+    switch(op){
+        case '+':
+            return sumar(num1, num2);
+        case '-':
+            return restar(num1, num2);
+        case '*':
+            return multiplicar(num1, num2);
+        case '/':
+            return dividir(num1, num2)
+        default:
+            return 'Opción inválida'
+    }
+}
